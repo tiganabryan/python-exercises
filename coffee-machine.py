f07f00306adc6fa8dc3f1c2a25c37b7b7997fa3e
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2023-01-28 08:56:48 -0500

    add resource report

--- coffee-machine.py
+++ coffee-machine.py
@@ -1,10 +1,10 @@
 import sys
 
 resources = {
-    "Water": 100,
-    "Milk": 50,
-    "Coffee": 76,
-    "Money": 2.5,
+    "water": 100,
+    "milk": 50,
+    "coffee": 76,
+    "money": 2.50,
 }
 
 # for resource, amount in resources.items():
@@ -16,31 +16,22 @@ is_on = True
 
 def take_order():
     global is_on
+
     while is_on:
         order = input("what would you like? (espresso/latte/cappuccino): ")
 
         if order == "espresso" or order == "latte" or order == "cappucino":
             print(is_on)
             print(f"1 {order} coming up!")
-        elif order == "stop":
+
+        elif order == "off":
             is_on = False
+
+        elif order == "report":
+            print(f"water: {resources['water']}ml")
+            print(f"milk: {resources['milk']}ml")
+            print(f"coffee: {resources['coffee']}g")
+            print(f"money: ${resources['money']}")
 
 
 take_order()
-
-
-# def take_order():
-#     order = input("what would you like? (espresso/latte/cappuccino): ")
-
-#     if order == "espresso" or "latte" or "cappucino":
-#         print(f"1 {order} coming up!")
-
-#     elif order == "stop":
-#         sys.exit()
-
-#     elif order == "report":
-#         for field in resources:
-#             print(f"{field}: {field.value}")
-
-
-# take_order()
